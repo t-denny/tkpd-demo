edddb456928ac54359cd9a96e35ae33465bd01ce
arsyaprasamya <arsyaprasamya@gmail.com>
2020-07-25 16:29:02 +0700

    fourth commit

--- src/com/workshop/Config.groovy
+++ src/com/workshop/Config.groovy
@@ -2,5 +2,12 @@
 package com.workshop
  
 class Config {
-   // TODO: Add Default Variable
+// Docker related default variable
+   def default_docker_registry = "https://registry-1.docker.io/v2/"
+   def default_docker_registry_jenkins_cred = 'cred-docker'
+   def default_docker_jenkins_tool = 'docker'
+ 
+   // Golang related default variable
+   def default_golang_base_image = "tobapramudia/tkpd-demo:onbuild"
+}
 }
--- src/com/workshop/Pipeline.groovy
+++ src/com/workshop/Pipeline.groovy
@@ -9,6 +9,8 @@ class Pipeline {
    String docker_user
    String app_port
    String pr_num
+   String dockerTool
+   String docker_registry
  
    Pipeline(
        String repository_name,
@@ -16,7 +18,9 @@ class Pipeline {
        String git_user,
        String docker_user,
        String app_port,
-       String pr_num
+       String pr_num,
+       String dockerTool,
+       String docker_registry
    ){
        this.repository_name = repository_name
        this.branch_name = branch_name
@@ -24,6 +28,8 @@ class Pipeline {
        this.docker_user = docker_user
        this.app_port = app_port
        this.pr_num = pr_num
+       this.dockerTool = dockerTool
+       this.docker_registry = docker_registry
    }
  
 }
