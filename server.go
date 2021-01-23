4f4ad4361b1df86c38caa3acb6f174ad6b673bfa
Andika Ahmad Ramadhan <andikahmadr@gmail.com>
2021-01-23 16:24:43 +0700

    update hello world

--- server.go
+++ server.go
@@ -29,7 +29,7 @@ func main() {
 
 // Handler
 func hello(c echo.Context) error {
-  return c.String(http.StatusOK, "Hello, Andika!")
+  return c.String(http.StatusOK, "Hello, Andika Ahmad!")
 }
 
 func ping(c echo.Context) error {
